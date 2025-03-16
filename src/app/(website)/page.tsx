aa9eabae68f509547f6968715537b63851dbb44b
amir-shz <amirsharif2079@gmail.com>
2025-03-16 22:37:25 +0330

    fix production bug

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -4,7 +4,9 @@ import FaqSection from "@/components/website/FaqSection";
 import HeroSection from "@/components/website/HeroSection";
 import ProductsSection from "@/components/website/ProductsSection";
 
-export default function HomePage() {
+export const revalidate = 0;
+
+export default async function HomePage() {
   return (
     <div className=" col-span-full flex flex-col gap-10">
       <HeroSection />
