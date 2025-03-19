2b6ab805e1568c10b12c94f59ca9b5aa78e3ec05
amir-shz <amirsharif2079@gmail.com>
2025-03-19 12:45:35 +0330

    fix pictures modal

--- src/components/website/CarPictures.tsx
+++ src/components/website/CarPictures.tsx
@@ -2,20 +2,9 @@
 
 import Image from "next/image";
 import { useState } from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogOverlay,
-  DialogPortal,
-  DialogTitle,
-} from "../ui/dialog";
-import { HiX } from "react-icons/hi";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/thumbs";
-import { Navigation, Thumbs } from "swiper/modules";
-import { Swiper as SwiperType } from "swiper/types";
-// import { NextSliderButton, PrevSliderButton } from "../SliderButton";
+import PicturesModal from "./PicturesModal";
 
 function CarPictures({
   pictures,
@@ -24,8 +13,6 @@ function CarPictures({
 }) {
   const [activePic, setActivePic] = useState<string | null>(null);
   const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const [thumbsSwiper, setThumbsSwiper] = useState<null | SwiperType>(null);
 
   return (
     <>
@@ -76,113 +63,11 @@ function CarPictures({
       </div>
 
       {isOpen && (
-        <Dialog open={isOpen} onOpenChange={setIsOpen}>
-          <DialogPortal>
-            <DialogOverlay className="fixed inset-0 bg-neutral-50/[0.01] dialog__overlay">
-              <DialogContent
-                style={{ maxWidth: "1280px" }}
-                className="[&>button]:hidden backdrop-blur-sm w-[50rem] overflow-x-hidden"
-              >
-                <DialogTitle className="hidden"></DialogTitle>
-                <div className=" grid grid-cols-1  w-full">
-                  <div className="flex flex-row-reverse">
-                    <HiX
-                      className=" size-6 cursor-pointer"
-                      onClick={() => setIsOpen(false)}
-                    />
-                  </div>
-                  <div className=" col-span-full pt-6 px-6 pb-3 rounded-xl ">
-                    {/* اسلایدر اصلی */}
-                    <Swiper
-                      spaceBetween={10}
-                      navigation
-                      thumbs={{ swiper: thumbsSwiper }}
-                      modules={[Navigation, Thumbs]}
-                      className="w-full h-[20rem] rounded-lg"
-                    >
-                      {pictures.map((picture, index) => (
-                        <SwiperSlide key={picture._id} className=" h-full">
-                          <Image
-                            width={1280}
-                            height={1280}
-                            src={picture.src}
-                            alt={`Car ${index + 1}`}
-                            className=" h-full object-cover object-center w-full"
-                          />
-                        </SwiperSlide>
-                      ))}
-                    </Swiper>
-
-                    {/* اسلایدر کوچک */}
-                    <Swiper
-                      onSwiper={setThumbsSwiper}
-                      spaceBetween={24}
-                      slidesPerView={6}
-                      freeMode
-                      watchSlidesProgress
-                      modules={[Thumbs]}
-                      dir="ltr"
-                      style={{
-                        overflowX: "scroll",
-                        overflowY: "visible",
-                        marginRight: "-64px",
-                        touchAction: "auto",
-                      }}
-                      className=" mt-3 cursor-pointer"
-                    >
-                      {pictures.map((picture, index) => (
-                        <SwiperSlide key={picture._id}>
-                          <Image
-                            width={640}
-                            height={640}
-                            src={picture.src}
-                            alt={`Thumbnail ${index + 1}`}
-                            className="h-12 w-28 rounded-lg object-cover border-2 border-transparent"
-                          />
-                        </SwiperSlide>
-                      ))}
-                    </Swiper>
-                    {/* <Swiper
-                    // onSlideChange={handleSlideChange}
-                    slidesPerView={1}
-                    spaceBetween={0}
-                    grabCursor={true}
-                    centeredSlides={true}
-                    modules={[Navigation]}
-                    className=" relative z-50 mx-auto h-[20rem] overflow-hidden"
-                    >
-                    {pictures.length === 0 && (
-                      <SwiperSlide className=" w-full bg-primary-50">
-                      <p className=" flex justify-center items-center h-full">
-                      تصویری وجود ندارد
-                      </p>
-                      </SwiperSlide>
-                      )}
-                      {pictures.map((picture) => (
-                        <SwiperSlide key={picture._id} className=" h-full">
-                        <Image
-                        src={picture.src}
-                        alt="car"
-                        width={1280}
-                        height={1280}
-                          className=" h-full object-cover object-center w-full"
-                        />
-                      </SwiperSlide>
-                    ))}
-                    
-                    {pictures.length > 1 && (
-                      <>
-                      <NextSliderButton />
-                      <PrevSliderButton />
-                      </>
-                      )}
-                      </Swiper> */}
-                  </div>
-                </div>
-              </DialogContent>
-            </DialogOverlay>
-          </DialogPortal>
-        </Dialog>
+        <PicturesModal
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          pictures={pictures}
+        />
       )}
     </>
   );
---
+++ src/components/website/PicturesModal.tsx
@@ -0,0 +1,134 @@
+import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+} from "../ui/dialog";
+import {
+  HiOutlineChevronLeft,
+  HiOutlineChevronRight,
+  HiX,
+} from "react-icons/hi";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Navigation, Thumbs } from "swiper/modules";
+import Image from "next/image";
+import IconButton from "../IconButton";
+import { Swiper as swiperType } from "swiper/types";
+
+function PicturesModal({
+  pictures,
+  isOpen,
+  setIsOpen,
+}: {
+  pictures: { src: string; _id: string }[];
+  isOpen: boolean;
+  setIsOpen: (val: boolean) => void;
+}) {
+  const [thumbsSwiper, setThumbsSwiper] = useState<null | swiperType>(null);
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogPortal>
+        <DialogOverlay className="fixed inset-0 bg-neutral-50/[0.01] dialog__overlay">
+          <DialogContent
+            style={{ maxWidth: "1280px" }}
+            className="[&>button]:hidden backdrop-blur-sm w-[50rem] overflow-x-hidden"
+          >
+            <DialogTitle className="hidden"></DialogTitle>
+            <div className=" grid grid-cols-1 w-full">
+              <div className=" flex flex-row-reverse">
+                <HiX
+                  className=" size-6 cursor-pointer"
+                  onClick={() => setIsOpen(false)}
+                />
+              </div>
+              <div className=" col-span-full rounded-xl p-6 pb-3">
+                {/* main slider */}
+                <Swiper
+                  grabCursor={true}
+                  thumbs={{ swiper: thumbsSwiper }}
+                  modules={[Navigation, Thumbs]}
+                  dir="ltr"
+                  className="relative w-full h-[20rem] rounded-lg"
+                >
+                  {pictures.map((picture, index) => (
+                    <SwiperSlide key={picture._id} className=" h-full">
+                      <Image
+                        width={1280}
+                        height={1280}
+                        src={picture.src}
+                        alt={`Car ${index + 1}`}
+                        className=" h-full object-cover object-center w-full"
+                      />
+                    </SwiperSlide>
+                  ))}
+                  {pictures.length > 1 && (
+                    <>
+                      <NextBtn />
+                      <PrevBtn />
+                    </>
+                  )}
+                </Swiper>
+
+                {/* mini slider */}
+                <Swiper
+                  onSwiper={setThumbsSwiper}
+                  slidesPerView={6}
+                  spaceBetween={24}
+                  freeMode={true}
+                  modules={[Thumbs]}
+                  dir="ltr"
+                  style={{
+                    overflowX: "scroll",
+                    overflowY: "visible",
+                    marginRight: "-64px",
+                    touchAction: "auto",
+                  }}
+                  className=" mt-3 cursor-pointer hide-scrollbar"
+                >
+                  {pictures.map((picture, index) => (
+                    <SwiperSlide key={picture._id}>
+                      <Image
+                        width={640}
+                        height={640}
+                        src={picture.src}
+                        alt={`Thumbnail ${index + 1}`}
+                        className="h-12 w-28 rounded-lg object-cover border-2 border-transparent"
+                      />
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              </div>
+            </div>
+          </DialogContent>
+        </DialogOverlay>
+      </DialogPortal>
+    </Dialog>
+  );
+}
+
+export default PicturesModal;
+
+function NextBtn() {
+  const swiper = useSwiper();
+  return (
+    <div className=" absolute top-1/2 -translate-y-1/2 left-3 z-30">
+      <IconButton size="lg" onClick={() => swiper.slideNext()}>
+        <HiOutlineChevronLeft />
+      </IconButton>
+    </div>
+  );
+}
+
+function PrevBtn() {
+  const swiper = useSwiper();
+  return (
+    <div className=" absolute top-1/2 -translate-y-1/2 right-3 z-30">
+      <IconButton size="lg" onClick={() => swiper.slidePrev()}>
+        <HiOutlineChevronRight />
+      </IconButton>
+    </div>
+  );
+}
