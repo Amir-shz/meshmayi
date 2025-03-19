3b440f0ade6a17a3bc75fac61fc84e4fbf5f3a07
amir-shz <amirsharif2079@gmail.com>
2025-03-19 23:42:08 +0330

    add loaders

---
+++ src/app/(website)/products/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className=" col-span-full  h-[calc(100dvh-18rem)] max-sm:h-[calc(100dvh-2.875em)]  flex justify-center items-center">
+      <div className="spinner"></div>
+    </div>
+  );
+}
