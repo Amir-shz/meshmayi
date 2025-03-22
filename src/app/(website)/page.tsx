6153de54d1da12bdbb326f4730542612885226ea
amir-shz <amirsharif2079@gmail.com>
2025-03-22 17:47:20 +0330

    change skelton loader

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -18,18 +18,25 @@ export default function HomePage() {
       <HeroSection />
       <Suspense
         fallback={
-          <div className=" grid grid-cols-2 sm:grid-cols-3 sm:gap-8 gap-3 [&>div]:rounded-lg [&>div]:w-full [&>div]:h-[282px] sm:[&>div]:h-[400px] [&>div]:bg-neutral-200 [&>div]:animate-pulse">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div className=" max-sm:hidden"></div>
-            <div className=" max-sm:hidden"></div>
-          </div>
+          <>
+            <div className=" sm:grid max-sm:flex max-sm:shrink-0 sm:grid-cols-3 sm:gap-8 gap-3 [&>div]:rounded-lg max-sm:[&>div]:min-w-[249px] sm:w-full max-sm:overflow-hidden max-sm:-ml-4 [&>div]:h-[282px] sm:[&>div]:h-[400px] max-sm:flex-nowrap [&>div]:bg-neutral-200 [&>div]:animate-pulse">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div className=" max-sm:hidden"></div>
+              <div className=" max-sm:hidden"></div>
+            </div>
+            <div className=" sm:hidden -mt-8 max-sm:flex max-sm:shrink-0 gap-3 [&>div]:rounded-lg max-sm:[&>div]:min-w-[249px] [&>div]:h-[282px] max-sm:overflow-hidden max-sm:-ml-4 max-sm:flex-nowrap [&>div]:bg-neutral-200 [&>div]:animate-pulse">
+              <div></div>
+              <div></div>
+            </div>
+          </>
         }
       >
         <ProductsSection />
       </Suspense>
+
       <AboutUsSection />
       <ContactUsSection />
       <FaqSection />
