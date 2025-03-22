d17be56fa6e8ab565864510279c8040ec0aefb29
amir-shz <amirsharif2079@gmail.com>
2025-03-22 17:54:54 +0330

    fix skelton loader

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -3,6 +3,7 @@ import ContactUsSection from "@/components/website/ContactUsSection";
 import FaqSection from "@/components/website/FaqSection";
 import HeroSection from "@/components/website/HeroSection";
 import ProductsSection from "@/components/website/ProductsSection";
+import SectionsHeader from "@/components/website/SectionsHeader";
 import { Metadata } from "next";
 import { Suspense } from "react";
 
@@ -19,6 +20,11 @@ export default function HomePage() {
       <Suspense
         fallback={
           <>
+            <SectionsHeader
+              title="محصولات"
+              href="/products"
+              linkTitle="محصولات بیشتر"
+            />
             <div className=" sm:grid max-sm:flex max-sm:shrink-0 sm:grid-cols-3 sm:gap-8 gap-3 [&>div]:rounded-lg max-sm:[&>div]:min-w-[249px] sm:w-full max-sm:overflow-hidden max-sm:-ml-4 [&>div]:h-[282px] sm:[&>div]:h-[400px] max-sm:flex-nowrap [&>div]:bg-neutral-200 [&>div]:animate-pulse">
               <div></div>
               <div></div>
