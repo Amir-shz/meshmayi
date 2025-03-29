a7571d3beb523ae455ae09e4b7655fd89b30a79f
amir-shz <amirsharif2079@gmail.com>
2025-03-29 21:19:56 +0330

    fix loader and create mobile menu

--- src/app/(website)/products/loading.tsx
+++ src/app/(website)/products/loading.tsx
@@ -1,22 +1,25 @@
+"use client";
+
 import SectionsHeader from "@/components/website/SectionsHeader";
+import { usePathname } from "next/navigation";
 
 export default function Loading() {
-  return (
-    // <div className=" col-span-full  h-[calc(100dvh-18rem)] max-sm:h-[calc(100dvh-12rem)]  flex justify-center items-center">
-    //   <div className="spinner"></div>
-    // </div>
-    <>
-      <div className=" col-span-full">
-        <SectionsHeader title="محصولات" />
-      </div>
-      <div className=" col-span-full grid grid-cols-1 sm:grid-cols-2 min-[850px]:grid-cols-3  gap-3 lg:gap-8 md:gap-6 [&>div]:h-[400px] [&>div]:bg-neutral-200 [&>div]:animate-pulse [&>div]:rounded-lg">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </>
-  );
+  const pathName = usePathname();
+
+  if (pathName === "/products")
+    return (
+      <>
+        <div className=" col-span-full">
+          <SectionsHeader title="محصولات" />
+        </div>
+        <div className=" col-span-full grid grid-cols-1 sm:grid-cols-2 min-[850px]:grid-cols-3  gap-3 lg:gap-8 md:gap-6 [&>div]:h-[400px] [&>div]:bg-neutral-200 [&>div]:animate-pulse [&>div]:rounded-lg">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </>
+    );
 }
--- src/components/website/HeaderBtn.tsx
+++ src/components/website/HeaderBtn.tsx
@@ -2,8 +2,20 @@
 
 import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
-import { HiOutlineArrowSmRight, HiOutlineMenu } from "react-icons/hi";
+import { HiOutlineArrowSmRight, HiOutlineMenu, HiX } from "react-icons/hi";
 import IconButton from "../IconButton";
+
+import { DialogTitle } from "@radix-ui/react-dialog";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerOverlay,
+  DrawerPortal,
+  DrawerTrigger,
+} from "../ui/drawer";
+
+import { NAV_ITEMS } from "@/utils/utils";
+import NavItem from "./NavItem";
 
 function HeaderBtn() {
   const pathName = usePathname();
@@ -11,17 +23,54 @@ function HeaderBtn() {
   const router = useRouter();
 
   return (
-    <div className=" lg:hidden flex items-center justify-center">
-      {pathName === "/" ? (
-        <button onClick={() => setIsOpen(!isOpen)}>
-          <HiOutlineMenu className=" size-6" />
-        </button>
-      ) : (
-        <IconButton size="lg" onClick={() => router.back()}>
-          <HiOutlineArrowSmRight />
-        </IconButton>
-      )}
-    </div>
+    <>
+      <div className=" lg:hidden flex items-center justify-center">
+        {pathName === "/" ? (
+          <button onClick={() => setIsOpen(!isOpen)}>
+            <HiOutlineMenu className=" size-6" />
+          </button>
+        ) : (
+          <IconButton size="lg" onClick={() => router.back()}>
+            <HiOutlineArrowSmRight />
+          </IconButton>
+        )}
+      </div>
+
+      {/* mobile menu */}
+      <div className="sm:hidden">
+        <Drawer direction="top" open={isOpen} onOpenChange={setIsOpen}>
+          <DrawerTrigger aria-label="menu"></DrawerTrigger>
+          <DrawerPortal>
+            <DrawerOverlay className="fixed inset-0 bg-neutral-50/5">
+              <DrawerContent className="top-0 w-[96dvw] mx-auto h-min mt-0 rounded-b-lg rounded-t-none [&>div:first-child]:hidden bg-neutral-100 border border-t-0 border-primary-300 shadow-[0px_4px_8px_0px_rgba(23,23,23,0.08)]">
+                <DialogTitle></DialogTitle>
+                <div className="flex justify-between items-center mx-3 mt-6">
+                  <button
+                    className="[&>svg]:size-6"
+                    onClick={() => setIsOpen(false)}
+                  >
+                    <HiX />
+                  </button>
+                </div>
+                <div className=" w-max flex flex-col mx-auto -mt-4 mb-6">
+                  {NAV_ITEMS.filter((el) => el.id !== "home")
+                    .reverse()
+                    .map((navItem) => (
+                      <button
+                        key={navItem.id}
+                        className=" py-3 px-6"
+                        onClick={() => setIsOpen(false)}
+                      >
+                        <NavItem id={navItem.id}>{navItem.title}</NavItem>
+                      </button>
+                    ))}
+                </div>
+              </DrawerContent>
+            </DrawerOverlay>
+          </DrawerPortal>
+        </Drawer>
+      </div>
+    </>
   );
 }
 
