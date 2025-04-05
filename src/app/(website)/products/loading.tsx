1ea2dbcd138f209e1afb9ffcb468fa82b4fddd63
amir-shz <amirsharif2079@gmail.com>
2025-04-05 11:47:16 +0330

    change loader

--- src/app/(website)/products/loading.tsx
+++ src/app/(website)/products/loading.tsx
@@ -24,9 +24,12 @@ export default function Loading() {
     );
   } else {
     return (
-      <div className=" col-span-full  h-[calc(100dvh-18rem)] max-sm:h-[calc(100dvh-12rem)]  flex justify-center items-center">
-        <div className="spinner"></div>
-      </div>
+      <>
+        <div className=" col-span-full h-[calc(100dvh-18rem)] max-sm:h-[calc(100dvh-12rem)]  flex justify-center items-center">
+          <div className="spinner"></div>
+        </div>
+        <div className="h-48"></div>
+      </>
     );
   }
 }
