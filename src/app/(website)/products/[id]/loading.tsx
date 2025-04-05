e6be418533ad66e490d09af13a853d2fdc9c4be9
amir-shz <amirsharif2079@gmail.com>
2025-04-05 11:37:43 +0330

    change loader

--- src/app/(website)/products/[id]/loading.tsx
+++ src/app/(website)/products/[id]/loading.tsx
@@ -1,7 +1,10 @@
 export default function Loading() {
   return (
-    <div className=" col-span-full  h-[calc(100dvh-18rem)] max-sm:h-[calc(100dvh-12rem)]  flex justify-center items-center">
-      <div className="spinner"></div>
-    </div>
+    <>
+      <div className=" col-span-full h-[calc(100dvh-18rem)] max-sm:h-[calc(100dvh-12rem)]  flex justify-center items-center">
+        <div className="spinner"></div>
+      </div>
+      <div className="h-48"></div>
+    </>
   );
 }
